8f38a38f3bfb6360359ece28328f52ea9cc8d44a
Ward Coessens <ward.coessens@gmail.com>
2013-12-20 08:03:56 +0100

    added tests for producten

--- producten/models.py
+++ producten/models.py
@@ -1,19 +1,44 @@
 from django.db import models
 
 
+class Eenheid(models.Model):
+    """Class staat voor een eenheid waarin een prijs kan uitgedrukt
+    worden. \ vb: Liter(L), Kubiek (Kb)"""
+
+    naam = models.CharField(max_length=255)
+    afkorting = models.CharField(max_length=5)
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.naam, self.afkorting)
+
+
 class Leverancier(models.Model):
     """Class die een leverancier voorsteld"""
+
     naam = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.naam, self.email)
 
 
-class SimpelProduct(models.Model):
+class Product(models.Model):
+    naam = models.CharField(max_length=255, unique=True)
+    beschrijving = models.CharField(max_length=4000, null=True)
+
+    class Meta:
+        abstract=True
+
+class SimpelProduct(Product):
     """Een simpel product. Simpel betekend hier niet samengesteld"""
+
     leverancier = models.ForeignKey(Leverancier)
     prijs = models.DecimalField(max_digits=6, decimal_places=2)
+    eenheid = models.ForeignKey(Eenheid)
 
-    naam = models.CharField(max_length=255)
-    beschrijving = models.CharField(max_length = 4000)
+    def __unicode__(self):
+        return "%s (%.2f/%s)" % (self.naam, self.prijs, self.eenheid.afkorting)
+
 
 class SamengesteldProductLijn(models.Model):
     """Een simpel product met aantal, dat deel is van een samengesteld
@@ -22,10 +47,12 @@ class SamengesteldProductLijn(models.Model):
     simpelProduct = models.ForeignKey(SimpelProduct)
     aantal = models.IntegerField()
 
-class SamengesteldProduct(models.Model):
+
+class SamengesteldProduct(Product):
     """Een samengesteld product bevat een aantal simpele producten, plus
     een aantal"""
 
     simpeleProducten = models.ManyToManyField(SamengesteldProductLijn)
-    naam = models.CharField(max_length=255)
-    beschrijving = models.CharField(max_length = 4000)
+
+    def __unicode__(self):
+        return "%s <%s>" % (self.naam, ','.join(self.simpeleProducten))
--- producten/tests.py
+++ producten/tests.py
@@ -1,3 +1,63 @@
+from django.db import IntegrityError
 from django.test import TestCase
+from logging import getLogger
+
+from producten.models import Leverancier, SamengesteldProductLijn, SamengesteldProduct,\
+    SimpelProduct, Eenheid
+
+logger = getLogger('producten.tests')
 
 # Create your tests here.
+class ProductTestCase(TestCase):
+
+    def setUp(self):
+        self.h = Eenheid.objects.create(naam="uur", afkorting="h")
+        self.kub = Eenheid.objects.create(naam="Kubiek", afkorting="m3")
+        self.kg = Eenheid.objects.create(naam="kilogram", afkorting="kg")
+
+        self.vdba = Leverancier.objects.create(naam="VDBA", email="vdba@gmail.com")
+
+        SimpelProduct.objects.create(naam="beton", leverancier=self.vdba,
+                                     beschrijving="Zeer hard", prijs=54.0,
+                                     eenheid=self.kub)
+
+    def test_vind_product(self):
+        p = SimpelProduct.objects.filter(naam='beton')
+        self.assertIsNotNone(p)
+
+    def test_vind_leverancier(self):
+        l = Leverancier.objects.filter(email="vdba@gmail.com")
+        count = l.count()
+        self.assertEqual(count, 1)
+
+        self.assertEqual(l[0].naam, "VDBA")
+
+    def test_meerdere_leveranciers(self):
+        Leverancier.objects.create(naam="abc", email="abc@gmail.com")
+        Leverancier.objects.create(naam="123", email="xcv@gmail.com")
+        Leverancier.objects.create(naam="456", email="xz@gmail.com")
+
+        self.assertEqual(Leverancier.objects.count(), 4)
+        self.assertEqual(Leverancier.objects.exclude(naam__contains='4').count(), 3)
+
+    def test_uniek_email(self):
+        Leverancier.objects.create(naam="abc", email="abc@gmail.com")
+        with self.assertRaises(IntegrityError):
+            Leverancier.objects.create(naam="123", email="abc@gmail.com")
+
+    def test_simpel_product(self):
+        h = Eenheid.objects.get(naam='uur')
+        mh = SimpelProduct.objects.create(naam="man uur", beschrijving="Een man uur",
+                                          prijs=35.5, eenheid=h, leverancier=self.vdba)
+        self.assertEqual("man uur (35.50/h)", mh.__unicode__())
+
+    def test_dubbele_namen(self):
+        graszaad = SimpelProduct.objects.create(naam="graszaad", prijs=12,
+                                                eenheid=self.kg, leverancier=self.vdba)
+        gras_zaaien = SamengesteldProduct(naam="Gras zaaiend", )
+
+
+
+
+
+
