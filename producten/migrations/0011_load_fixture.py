a02f4f1808d60765ad3110378964c712d3c4b724
fahrrad <ward.coessens@gmail.com>
2013-12-30 22:08:14 +0100

    fixture toegevoegd

---
+++ producten/migrations/0011_load_fixture.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+        # Note: Don't use "from appname.models import ModelName". 
+        # Use orm.ModelName to refer to models in this application,
+        # and orm['appname.ModelName'] for models in other applications.
+        from django.core.management import call_command
+        call_command("loaddata", "producten/fixture.json")
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+    models = {
+        u'producten.eenheid': {
+            'Meta': {'object_name': 'Eenheid'},
+            'afkorting': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '15'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'naam': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        u'producten.leverancier': {
+            'Meta': {'object_name': 'Leverancier'},
+            'adres': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'contactpersoon': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '255', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'naam': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'telefoon': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        u'producten.samengesteldproduct': {
+            'Meta': {'ordering': "['naam']", 'object_name': 'SamengesteldProduct'},
+            'beschrijving': ('django.db.models.fields.CharField', [], {'max_length': '4000', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'items': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['producten.SimpelProduct']", 'through': u"orm['producten.SamengesteldProductLijn']", 'symmetrical': 'False'}),
+            'naam': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'producten.samengesteldproductlijn': {
+            'Meta': {'object_name': 'SamengesteldProductLijn'},
+            'aantal': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'samengesteldProduct': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['producten.SamengesteldProduct']"}),
+            'simpelProduct': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['producten.SimpelProduct']"})
+        },
+        u'producten.simpelproduct': {
+            'Meta': {'unique_together': "(['naam', 'leverancier'],)", 'object_name': 'SimpelProduct'},
+            'beschrijving': ('django.db.models.fields.CharField', [], {'max_length': '4000', 'blank': 'True'}),
+            'eenheid': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['producten.Eenheid']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'laatste_aanpassing': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2013, 12, 30, 0, 0)', 'null': 'True'}),
+            'leverancier': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['producten.Leverancier']"}),
+            'minimum_hoeveelheid': ('django.db.models.fields.DecimalField', [], {'max_digits': '6', 'decimal_places': '2', 'blank': 'True'}),
+            'naam': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'nakijken': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'prijs': ('django.db.models.fields.DecimalField', [], {'max_digits': '6', 'decimal_places': '2'})
+        }
+    }
+
+    complete_apps = ['producten']
+    symmetrical = True
